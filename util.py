d736d6546a3dd7d02ac9584d2adc88b1453b3c3a
Ryan Kortvelesy <rk627@cam.ac.uk>
2022-09-02 15:04:37 +0100

    added collect

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def scatter_nested(input, idxi, idxj):
 	idxi, perm = torch.sort(idxi)
 	idxj = idxj[perm]
@@ -9,6 +10,7 @@ def scatter_nested(input, idxi, idxj):
 	out = torch.nested_tensor(torch.split(out_flat, sizes.tolist()))
 	return out
 
+
 def sum_nested(input, dim):
 	dim = dim - 1 if dim > 0 else dim
 	x = [xi.sum(dim=dim) for xi in input.unbind()]
@@ -17,11 +19,13 @@ def sum_nested(input, dim):
 	else:
 		return torch.nested_tensor(x)
 
+
 def size_nested(input, dim):
 	if dim == 0:
 		return len(input._nested_tensor_size())
 	dim = dim - 1 if dim > 0 else dim
 	return torch.tensor([size[dim] for size in input._nested_tensor_size()])
+
 
 def cat_nested(input1, input2, dim):
 	if dim == 0:
@@ -42,5 +46,29 @@ def cat_nested(input1, input2, dim):
 			],dim=dim)
 		for (x1i, x2i) in zip(input1.unbind(), input2.unbind())])
 
+
 def mul_nested(input1, input2):
 	return torch.nested_tensor([x1i * x2i for (x1i, x2i) in zip(input1.unbind(), input2.unbind())])
+
+
+def collect(input, index, dim_along=0, dim_select=1):
+	'''
+	input is the source tensor, of any shape
+	index is a 1D tensor of shape input.shape[dim_along]
+	dim_along is the dimension where each element you would like a different value
+	dim_select is the dimension that elements in index are indexing
+
+	For example, if you have input: (B, T, D) and index: (B,) where each element is in range(0, T),
+	you can select the t-th element from each row in the batch dimension to produce an output of shape (B, D) with the following:
+	output = collect(input, index, along_dim=0, dim_select=1)
+	because we are indexing along dimension 0 (the B dim), and each element in index picks out an element in dimension 1 (the T dim).
+	'''
+	shape = list(input.shape)
+	shape[dim_along] = index.shape[0]
+	shape[dim_select] = 1
+	unsqueeze_shape = [1] * len(shape)
+	unsqueeze_shape[dim_along] = index.shape[0]
+	index_unsqueezed = index.view(*unsqueeze_shape)
+	index_expanded = index_unsqueezed.expand(*shape)
+	out = torch.gather(input=input, index=index_expanded, dim=dim_select)
+	return out.squeeze(dim_select)
